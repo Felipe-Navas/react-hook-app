db2eed48c372a41fac8cec461b4fb0a504d1ad94
Felipe Navas <felipenavas.itec@gmail.com>
2022-03-28 17:06:45 -0300

    Adding feature for add a Todo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,30 +1,90 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm';
 import './styles.css';
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    desc: 'Deploy an application',
-    done: false,
-  },
-];
+const init = () => {
+  // return [
+  //   {
+  //     id: new Date().getTime(),
+  //     desc: 'Deploy an application',
+  //     done: false,
+  //   },
+  // ];
+
+  return JSON.parse(localStorage.getItem('Todos')) || [];
+};
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-  console.log(todos);
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: '',
+  });
+
+  useEffect(() => {
+    localStorage.setItem('Todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (description.trim().length <= 1) {
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+    const action = {
+      type: 'add',
+      payload: newTodo,
+    };
+    dispatch(action);
+    reset();
+  };
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
 
-      <ul>
-        <li>First task</li>
-        <li>Second task</li>
-        <li>Third task</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-danger">Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control"
+              type="text"
+              name="description"
+              placeholder="Enter a todo description"
+              autoComplete="off"
+              onChange={handleInputChange}
+              value={description}
+            />
+            <div className="d-grid gap-2">
+              <button className="btn btn-outline-success mt-1" type="submit">
+                Agregar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
