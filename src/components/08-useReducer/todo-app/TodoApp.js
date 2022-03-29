42e663db2cf216835d320856a0984b0ce06308ad
Felipe Navas <felipenavas.itec@gmail.com>
2022-03-29 09:39:53 -0300

    Refactoring the Add todo

--- src/components/08-useReducer/todo-app/TodoApp.js
+++ src/components/08-useReducer/todo-app/TodoApp.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
-import { useForm } from '../../../hooks/useForm';
 import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 import './styles.css';
 
 const init = () => {
@@ -18,10 +18,6 @@ const init = () => {
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: '',
-  });
 
   useEffect(() => {
     localStorage.setItem('Todos', JSON.stringify(todos));
@@ -43,24 +39,12 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim().length <= 1) {
-      return;
-    }
-
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
+  const handleAddTodo = (newTodo) => {
     const action = {
       type: 'add',
       payload: newTodo,
     };
     dispatch(action);
-    reset();
   };
 
   return (
@@ -77,24 +61,7 @@ export const TodoApp = () => {
           />
         </div>
         <div className="col-5">
-          <h4>Add TODO</h4>
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              className="form-control"
-              type="text"
-              name="description"
-              placeholder="Enter a todo description"
-              autoComplete="off"
-              onChange={handleInputChange}
-              value={description}
-            />
-            <div className="d-grid gap-2">
-              <button className="btn btn-success mt-1" type="submit">
-                Add Todo
-              </button>
-            </div>
-          </form>
+          <TodoAdd handleAddTodo={handleAddTodo} />
         </div>
       </div>
     </div>
