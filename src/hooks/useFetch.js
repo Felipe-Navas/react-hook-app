6b49be7b9cf257bd02be015c6308cb4a051313b2
Felipe Navas <felipenavas.itec@gmail.com>
2022-03-31 14:51:58 -0300

    Adding test for custom hook useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -33,6 +33,13 @@ export const useFetch = (url) => {
             data,
           });
         }
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error: 'No se pudo obtener la informacion',
+        });
       });
   }, [url]);
 
